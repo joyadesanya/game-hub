beef23fd34b5b89340ae0f80ede6796ee5d0faa7
Joy Adesanya <posiogunti@gmail.com>
2024-02-20 11:20:20 +0100

    Build a responsive layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./NavBar";
 
 function App() {
-  return <NavBar />;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem bg="coral" area="nav">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="gold" area="aside">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="blue" area="main">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
