7c38bc800875517ad964463a20b36b876ecc4a6d
Joy Adesanya <posiogunti@gmail.com>
2024-02-20 11:24:49 +0100

    Update the NavBar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Image, HStack } from "@chakra-ui/react";
+import { Image, HStack, Text } from "@chakra-ui/react";
 import logo from "./assets/logo.webp";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
-      <button>Submit</button>
+      <Text>NavBar</Text>
     </HStack>
   );
 };
