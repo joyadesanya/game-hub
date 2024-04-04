3876d12edb8a634aff8194c864742c465fc46356
Joy Adesanya <posiogunti@gmail.com>
2024-04-04 11:35:52 +0200

    Fetch game details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,14 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={30} overflow="hidden">
+    <Box
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+      borderRadius={30}
+      overflow="hidden"
+    >
       {children}
     </Box>
   );
