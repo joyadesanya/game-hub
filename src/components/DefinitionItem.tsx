46023bab208ebf188a32b2efe868db542ea46d87
Joy Adesanya <posiogunti@gmail.com>
2024-04-04 15:16:35 +0200

    Build game attributes

---
+++ src/components/DefinitionItem.tsx
@@ -0,0 +1,21 @@
+import { Box, Heading } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+
+interface Props {
+  term: string;
+  children: ReactNode | ReactNode[];
+}
+
+const DefinitionItem = ({ term, children }: Props) => {
+  // dt is definition term, dd is definition description in HTML
+  return (
+    <Box margin={5}>
+      <Heading as="dt" fontSize="md" color="gray.600">
+        {term}
+      </Heading>
+      <dd>{children}</dd>
+    </Box>
+  );
+};
+
+export default DefinitionItem;
